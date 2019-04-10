671e69d446e1b5f2a9f4c02853959719aaa83ff2
Mason Davis <mason.davis@utexas.edu>
2019-04-10 15:33:57 -0500

    fixed link pathing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=(".Xresources", ".vimrc", ".vim/configs/binds.vim", ".vim/configs/color.vim", ".vim/configs/plugins.vim", ".vim/configs/settings.vim", '.tmux.conf')
+FILES=(".Xresources" ".vimrc" ".vim/configs/binds.vim" ".vim/configs/color.vim" ".vim/configs/plugins.vim" ".vim/configs/settings.vim" '.tmux.conf')
 DIRS=(".vim/configs")
 
 echo ' ___    __  ____  ___  __  __    ___  ___ '
@@ -12,12 +12,12 @@ echo
 
 for dir in "${DIRS[@]}"; do
   echo "Creating Directory: $dir"
-  mkdir -p $dir
+  mkdir -p $HOME/$dir
 done
 
 echo
 
 for i in "${FILES[@]}"; do
   echo "Linking Dotfile: $i"
-  ln -s -f $i $HOME/$i
+  ln -s -f $PWD/$i $HOME/$i
 done
