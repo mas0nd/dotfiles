7dd1de145972a26ecb7629d2b3ed048f3b3cd481
Mason Davis <mason.davis@utexas.edu>
2019-04-10 15:23:17 -0500

    base setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILES=(".Xresources", ".vimrc", ".vim/configs/binds.vim", ".vim/configs/color.vim", ".vim/configs/plugins.vim", ".vim/configs/settings.vim")
+DIRS=(".vim/configs")
+
+echo ' ___    __  ____  ___  __  __    ___  ___ '
+echo '(   \  /  \(_  _)(  _)(  )(  )  (  _)/ __)'
+echo ' ) ) )( () ) )(   ) _) )(  )(__  ) _)\__ \'
+echo '(___/  \__/ (__) (_)  (__)(____)(___)(___/'
+
+echo
+
+for dir in "${DIRS[@]}"; do
+  echo "Creating Directory: $dir"
+  mkdir -p $dir
+done
+
+echo
+
+for i in "${FILES[@]}"; do
+  echo "Linking Dotfile: $i"
+  ln -s -f $i $HOME/$i
+done
